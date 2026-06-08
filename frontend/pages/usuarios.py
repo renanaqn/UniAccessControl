90733a35d1ca2d4c79f8f899b9d893a36fc883c4
Renan Aquino <renanap99@gmail.com>
2026-06-08 13:32:22 -0300

    add: implementacao das paginas de usuarios e zonas

--- frontend/pages/usuarios.py
+++ frontend/pages/usuarios.py
@@ -7,11 +7,48 @@ from components.log_table import log_table
 from states.dashboard_state import DashboardState
 
 def usuarios_page():
-    return rx.box(
-    "Vazio",
-    id="box-id",
-    class_name=[
-        "class-name-1",
-        "class-name-2",
-    ],
-)
+    return page_layout(
+        rx.vstack(
+            rx.heading("Gestão de Utilizadores", size="7", margin_bottom="5"),
+            rx.text("Cadastre novos indivíduos, atualize credenciais perdidas ou revogue acessos.", color="gray", margin_bottom="5"),
+            
+            rx.flex(
+                rx.card(
+                    rx.heading("Novo Utilizador", size="4", margin_bottom="4"),
+                    rx.vstack(
+                        rx.input(placeholder="Nome Completo", value=DashboardState.novo_nome, on_change=DashboardState.set_novo_nome),
+                        rx.input(placeholder="Código da Tag RFID", value=DashboardState.novo_rfid, on_change=DashboardState.set_novo_rfid),
+                        rx.input(placeholder="ID Perfil (1=Prof, 2=Aluno)", value=DashboardState.novo_perfil_id, on_change=DashboardState.set_novo_perfil_id),
+                        rx.button("Cadastrar", on_click=DashboardState.registrar_usuario, color_scheme="blue", width="100%"),
+                        rx.text(DashboardState.msg_cadastro, color="blue", size="2"),
+                    ),
+                    width="100%", max_width="350px"
+                ),
+                
+                rx.card(
+                    rx.heading("Atualizar Credencial", size="4", margin_bottom="4"),
+                    rx.vstack(
+                        rx.input(placeholder="ID do Utilizador", value=DashboardState.update_id, on_change=DashboardState.set_update_id),
+                        rx.input(placeholder="Nova Tag RFID", value=DashboardState.update_rfid, on_change=DashboardState.set_update_rfid),
+                        rx.button("Atualizar Tag", on_click=DashboardState.atualizar_tag, color_scheme="orange", width="100%"),
+                        rx.text(DashboardState.msg_update, color="orange", size="2"),
+                    ),
+                    width="100%", max_width="350px"
+                ),
+                
+                rx.card(
+                    rx.heading("Revogar Acesso", size="4", margin_bottom="4"),
+                    rx.vstack(
+                        rx.input(placeholder="ID a remover", value=DashboardState.delete_id, on_change=DashboardState.set_delete_id),
+                        rx.button("Remover do Sistema", on_click=DashboardState.remover_usuario, color_scheme="red", width="100%"),
+                        rx.text(DashboardState.msg_delete, color="red", size="2"),
+                    ),
+                    width="100%", max_width="350px"
+                ),
+                spacing="5",
+                wrap="wrap",
+                width="100%"
+            ),
+            width="100%"
+        )
+    )
--- frontend/states/dashboard_state.py
+++ frontend/states/dashboard_state.py
@@ -17,6 +17,47 @@ class DashboardState(rx.State):
 
     ultimos_logs: list[dict] = []
 
+    zonas_lista: list[dict] = []
+    perfis_lista: list[dict] = []
+
+    novo_nome: str = ""
+    novo_rfid: str = ""
+    novo_perfil_id: str = ""
+    msg_cadastro: str = ""
+
+    update_id: str = ""
+    update_rfid: str = ""
+    msg_update: str = ""
+
+    delete_id: str = ""
+    msg_delete: str = ""
+
+    regra_perfil_id: str = ""
+    regra_zona_id: str = ""
+    regra_inicio: str = ""
+    regra_fim: str = ""
+    msg_regra: str = ""
+
+
+    # ==========================================
+    # SETTERS EXPLÍCITOS (Evita o erro do Python 3.13)
+    # ==========================================
+    def set_novo_nome(self, v: str): self.novo_nome = v
+    def set_novo_rfid(self, v: str): self.novo_rfid = v
+    def set_novo_perfil_id(self, v: str): self.novo_perfil_id = v
+    
+    def set_update_id(self, v: str): self.update_id = v
+    def set_update_rfid(self, v: str): self.update_rfid = v
+    
+    def set_delete_id(self, v: str): self.delete_id = v
+    
+    def set_regra_perfil(self, v: str): self.regra_perfil_id = v
+    def set_regra_zona(self, v: str): self.regra_zona_id = v
+    def set_regra_inicio(self, v: str): self.regra_inicio = v
+    def set_regra_fim(self, v: str): self.regra_fim = v
+    # ==========================================
+
+
     def carregar_dados(self):
 
         db = BancoDeDados()
@@ -28,3 +69,48 @@ class DashboardState(rx.State):
 
         self.acessos_aprovados_hoje = db.acessos_permitidos_hoje()
         self.acessos_negados_hoje = db.acessos_negados_hoje()
+
+        self.zonas_lista = db.listar_zonas()
+        self.perfis_lista = db.listar_perfis()
+
+    def registrar_usuario(self):
+        db = BancoDeDados()
+        try:
+            sucesso, msg = db.cadastrar_usuario(self.novo_nome, self.novo_rfid, int(self.novo_perfil_id))
+            self.msg_cadastro = msg
+            if sucesso:
+                self.novo_nome = self.novo_rfid = self.novo_perfil_id = ""
+                self.carregar_dados()
+        except ValueError:
+            self.msg_cadastro = "Erro: ID e Perfil precisam ser números!"
+    
+    def atualizar_tag(self):
+        db = BancoDeDados()
+        try:
+            sucesso, msg = db.atualizar_tag(int(self.update_id), self.update_rfid)
+            self.msg_update = msg
+            if sucesso: self.update_id = self.update_rfid = ""
+        except ValueError:
+            self.msg_update = "Erro: ID precisa ser um número!"
+
+    def remover_usuario(self):
+        db = BancoDeDados()
+        try:
+            sucesso, msg = db.remover_usuario(int(self.delete_id))
+            self.msg_delete = msg
+            if sucesso:
+                self.delete_id = ""
+                self.carregar_dados()
+        except ValueError:
+            self.msg_delete = "Erro: ID precisa ser um número!"
+
+    def salvar_regra(self):
+        db = BancoDeDados()
+        try:
+            sucesso, msg = db.criar_regra_acesso(int(self.regra_perfil_id), int(self.regra_zona_id), self.regra_inicio, self.regra_fim)
+            self.msg_regra = msg
+            if sucesso:
+                self.regra_perfil_id = self.regra_zona_id = self.regra_inicio = self.regra_fim = ""
+        except ValueError:
+            self.msg_regra = "Erro: IDs precisam ser números válidos!"
+
